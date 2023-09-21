96bae552f94e8cad3d5069370d01d11803ff8e79
MuhdAdnan <144796851+MuhdAdnan@users.noreply.github.com>
2023-09-21 12:44:19 +0100

    Add files via upload
     my code of the day

---
+++ main.py
@@ -0,0 +1,29 @@
+# Adding two numbers
+# a = 9
+# b = 3
+# c = a + b
+# print(c)
+
+# subtracting two numbers
+# a = 9
+# b = 3
+# c = a - b
+# print(c)
+
+# dividing two numbers
+# a = 9
+# b = 3
+# c = a * b
+# print(c)
+
+# multiplying two numbers
+# a = 9
+# b = 3
+# c = a / b
+# print(c)
+
+# a = 'Python Language'
+# print(a[: 2])
+# print(a[2: 10])
+# print(a[: 6])
+# print(a[6: 11])
